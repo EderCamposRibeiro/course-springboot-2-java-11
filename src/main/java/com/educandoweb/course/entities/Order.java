87e813d8d13047433cfefeda21f6eba8912a7e7c
EderCamposRibeiro <edercribeiro@gmail.com>
2020-03-29 10:47:04 -0300

    Payment, one-to-one association

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -5,6 +5,7 @@ import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,6 +13,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.educandoweb.course.entities.enums.OrderStatus;
@@ -28,7 +30,7 @@ public class Order implements Serializable {
 	private Long id;
 	
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
-	private Instant monent;
+	private Instant moment;
 	
 	private Integer orderStatus;
 	
@@ -39,13 +41,16 @@ public class Order implements Serializable {
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
 	
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	private Payment payment;
+	
 	public Order() {
 	}
 
-	public Order(Long id, Instant monent, OrderStatus orderStatus, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
-		this.monent = monent;
+		this.moment = moment;
 		setOrderStatus(orderStatus);
 		this.client = client;
 	}
@@ -59,11 +64,11 @@ public class Order implements Serializable {
 	}
 
 	public Instant getMonent() {
-		return monent;
+		return moment;
 	}
 
-	public void setMonent(Instant monent) {
-		this.monent = monent;
+	public void setMonent(Instant moment) {
+		this.moment = moment;
 	}
 	
 	public OrderStatus getOrderStatus() {
@@ -90,6 +95,14 @@ public class Order implements Serializable {
 	
 	public Set<OrderItem> getItems() {
 		return items;
+	}
+	
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
 	}
 
 	@Override
